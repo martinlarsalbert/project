bfabfc295fdd941f24755cbc35365a30baa32edd
Martin Alexandersson <marale@kth.se>
2021-07-05 10:03:06 +0200

    and forgot these

--- tests/test_create_latex_repot.py
+++ tests/test_create_latex_repot.py
@@ -2,6 +2,8 @@ from src.notebook_to_latex import convert_notebook_to_latex
 import pytest
 import os
 import reports
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 
 @pytest.fixture
 def build_directory(tmpdir):
@@ -13,11 +15,21 @@ def build_directory(tmpdir):
 
     yield directory
 
+@pytest.fixture
+def notebook_path():
+    report_dir = os.path.join(reports.path,'report')
+    os.chdir(report_dir)
+    yield os.path.join(report_dir, '01.1.report.ipynb')
 
 
+def test_create_latex_report(notebook_path, build_directory):
 
-def test_create_latex_report(build_directory):
-
-    notebook_path = os.path.join(reports.path,'report','01.1.report.ipynb')
     convert_notebook_to_latex(notebook_path=notebook_path, build_directory=build_directory,save_main=True, skip_figures=False)
 
+def test_run_notebook(notebook_path):
+
+    with open(notebook_path) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    ep.preprocess(nb)
